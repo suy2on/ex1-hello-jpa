f01c4545994064f411a62b1160b4c92114fcccd1
suy2on <tnwjdrms@naver.com>
2022-01-17 16:48:11 +0900

    # feat : 영속성컨테이너 개념 실습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,10 +15,14 @@ public class JpaMain {
         //code
         try {
 //            생성
+//             > 비영속
 //            Member member = new Member();
 //            member.setId(2L);
 //            member.setName("HelloB");
+//             > 영속
 //            em.persist(member);
+//            > 영속해지
+//            em.detach(member);
 
 //            조회
 //            Member findMember = em.find(Member.class, 1L);
@@ -27,14 +31,22 @@ public class JpaMain {
 //            수정
 //            findMember.setName("HelloJPA");
 
-//            전체조회 : jpql
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
-            for (Member member : result){
-                System.out.println("member.name = " + member.getName());
-            }
+//              전체조회 : jpql
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .getResultList();
+//            for (Member member : result){
+//                System.out.println("member.name = " + member.getName());
+//            }
 
-            tx.commit();
+            // buffer
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("google");
+
+            em.flush();
+
+            System.out.println("==========");
+
+            tx.commit(); // DB에 쿼리가 날라가는 순간
         }catch (Exception e){
             tx.rollback();
         } finally {
@@ -43,4 +55,5 @@ public class JpaMain {
 
         emf.close();
     }
+
 }
