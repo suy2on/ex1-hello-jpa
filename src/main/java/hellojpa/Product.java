cd3315d83af4efad9c79976228f48a5eaecd97db
suy2on <tnwjdrms@naver.com>
2022-01-25 02:56:00 +0900

    # feat : 다대다 실습

---
+++ src/main/java/hellojpa/Product.java
@@ -0,0 +1,19 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Product {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+//    @ManyToMany(mappedBy = "products")
+//    private List<Member> members = new ArrayList<>();
+
+    @OneToMany(mappedBy = "product")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
+}
