630fc39d282b8c9c8ee0873dc90e5045cf4aa63c
suy2on <tnwjdrms@naver.com>
2022-01-26 16:39:38 +0900

    # feat : 프록시 객체 실습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,9 @@
 package hellojpa;
 
+import org.hibernate.Hibernate;
+
 import javax.persistence.*;
+import javax.swing.plaf.metal.MetalMenuBarUI;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -22,18 +25,47 @@ public class JpaMain {
 //            movie.setName("바함사");
 //            movie.setPrice(10000);
 
-            Member member = new Member();
-            member.setUsername("A");
-            member.setCreatedBy("B");
-            member.setCreatedDate(LocalDateTime.now());
-
-            em.persist(member);
-
-//            em.flush();
-//            em.clear();
-//
 //            Movie findMovie = em.find(Movie.class, movie.getId());
 //            System.out.println("findMovie = "+ findMovie );
+
+            Member member = new Member();
+            member.setUsername("A");
+//            member.setCreatedBy("B");
+//            member.setCreatedDate(LocalDateTime.now());
+            Member member2 = new Member();
+            member2.setUsername("B");
+
+            em.persist(member);
+            em.persist(member2);
+
+            em.flush();
+            em.clear();
+
+//            Member findMember2 = em.find(Member.class, member2.getId());
+
+            // 프록시객체 : 실제 클래스와 겉모양이 같음
+            Member findMember = em.getReference(Member.class, member.getId()); // 홀로는 쿼리 안날림
+
+
+            System.out.println("findMember = " + findMember.getClass());
+            System.out.println("findMember.getId = " + findMember.getId());
+
+            //Hibernate.initialize(findMember); 강제 초기화 가능
+
+            // 쓰일 때 쿼리날림 (초기화)
+            System.out.println("findMember.getUsername = " + findMember.getUsername());
+            // 초기화 되서 초기화 요청 안함
+            System.out.println("findMember.getUsername = " + findMember.getUsername());
+
+//            // == 은 모든것을 다 비교
+//            System.out.println("m1 == m2 :" + (findMember.getClass() == findMember2.getClass()));
+//
+//            // 타입 비교시에는 instancOf
+//            System.out.println("m1 class :" + (findMember instanceof Member ) );
+//            System.out.println("m2 class :" + (findMember2 instanceof Member ) );
+
+
+
 
             System.out.println("==========");
 
